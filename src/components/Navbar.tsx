20b95568cb1a1ddb8f8125c902e3d03ff5461bf2
Volodymyr Lykhonis <volodymyr@lykhonis.com>
2022-07-07 07:10:04 -0400

    Popover menu with profile description

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Fragment, ReactNode, RefObject, useRef, useState } from 'react'
+import { Fragment, ReactNode, RefObject, useCallback, useRef, useState } from 'react'
 import { Button, Chip } from '@apideck/components'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -7,8 +7,9 @@ import { useProfile } from 'hooks/useProfile'
 import { shortenAddress } from 'utils/shortenAddress'
 import { findBestProfileImage } from 'utils/profileImage'
 import { ipfsToHttp } from 'utils/ipfs'
-import { Menu, Transition } from '@headlessui/react'
-import classNames from 'classnames'
+import { Transition, Popover } from '@headlessui/react'
+import { useClipboard } from 'use-clipboard-copy'
+import { useToast } from '@apideck/components'
 
 export const Navbar = ({ children }: { children?: ReactNode }) => {
   const navbarNode = useRef() as RefObject<HTMLDivElement>
@@ -21,6 +22,14 @@ export const Navbar = ({ children }: { children?: ReactNode }) => {
     minimumWidth: 32,
     minimumHeight: 32
   })
+  const { addToast } = useToast()
+  const clipboard = useClipboard()
+  const handleCopyAddress = useCallback(() => {
+    if (clipboard && profile?.address) {
+      clipboard.copy(profile.address)
+      addToast({ title: 'Copied', type: 'success' })
+    }
+  }, [profile?.address, clipboard, addToast])
   return (
     <nav className={profile ? 'bg-white border-b-2 border-gray-100' : 'bg-gray-900'}>
       <div className="px-4 sm:px-6 lg:px-6 max-w-7xl mx-auto">
@@ -73,52 +82,98 @@ export const Navbar = ({ children }: { children?: ReactNode }) => {
                   </Button>
                 ) : (
                   <>
-                    <Menu as="div" className="ml-4 relative flex-shrink-0">
-                      <div>
-                        <Menu.Button className="bg-white rounded-full flex text-sm ring-2 ring-white ring-opacity-20 focus:outline-none focus:ring-opacity-100">
-                          <span className="sr-only">Open user menu</span>
-                          {profileImage ? (
-                            <img
-                              className="w-8 h-8 rounded-full"
-                              src={ipfsToHttp(profileImage.url)}
-                              alt=""
-                            />
-                          ) : profile.name ? (
-                            <span className="inline-flex items-center justify-center h-8 w-8 rounded-full bg-purple-500">
-                              <span className="text-sm font-medium leading-none text-white">
-                                {profile.name.slice(0, 1).toUpperCase()}
-                              </span>
+                    <Popover as="div" className="ml-4 relative flex-shrink-0">
+                      <Popover.Button className="bg-white rounded-full flex text-sm ring-2 ring-white ring-opacity-20 focus:outline-none focus:ring-opacity-100">
+                        {profileImage ? (
+                          <img
+                            className="w-8 h-8 rounded-full"
+                            src={ipfsToHttp(profileImage.url)}
+                            alt=""
+                          />
+                        ) : profile.name ? (
+                          <span className="inline-flex items-center justify-center h-8 w-8 rounded-full bg-purple-500">
+                            <span className="text-sm font-medium leading-none text-white">
+                              {profile.name.slice(0, 1).toUpperCase()}
                             </span>
-                          ) : (
-                            <div className="whitespace-nowrap ml-4 text-sm font-medium">
-                              {shortenAddress(profile.address ?? '')}
-                            </div>
-                          )}
-                        </Menu.Button>
-                      </div>
+                          </span>
+                        ) : (
+                          <div className="whitespace-nowrap ml-4 text-sm font-medium">
+                            {shortenAddress(profile.address ?? '')}
+                          </div>
+                        )}
+                      </Popover.Button>
                       <Transition
                         as={Fragment}
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
+                        enter="transition duration-100 ease-out"
+                        enterFrom="transform scale-95 opacity-0"
+                        enterTo="transform scale-100 opacity-100"
+                        leave="transition duration-75 ease-out"
+                        leaveFrom="transform scale-100 opacity-100"
+                        leaveTo="transform scale-95 opacity-0"
                       >
-                        <Menu.Items className="origin-top-right z-40 absolute -right-2 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <Menu.Item key="sign-out">
-                            {({ active }) => (
+                        <Popover.Panel className="origin-top-right z-40 absolute -right-2 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <div className="relative rounded-lg bg-white p-5 shadow-sm flex items-center space-x-3">
+                            <div className="flex-shrink-0">
+                              {profileImage ? (
+                                <img
+                                  className="h-10 w-10 rounded-full"
+                                  src={ipfsToHttp(profileImage.url)}
+                                  alt=""
+                                />
+                              ) : profile.name ? (
+                                <span className="inline-flex items-center justify-center h-8 w-8 rounded-full bg-purple-500">
+                                  <span className="text-sm font-medium leading-none text-white">
+                                    {profile.name.slice(0, 1).toUpperCase()}
+                                  </span>
+                                </span>
+                              ) : (
+                                <></>
+                              )}
+                            </div>
+                            <div className="flex-1 min-w-0">
+                              <p className="text-sm font-medium text-gray-900">
+                                {profile?.name ?? ''}
+                              </p>
+                              <div className="flex flex-row">
+                                <p className="text-sm text-gray-500 truncate">
+                                  {shortenAddress(profile?.address ?? '', 2)}
+                                </p>
+                                <a className="ml-1" onClick={handleCopyAddress}>
+                                  <svg
+                                    className="h-5 w-5 text-gray-500 hover:text-gray-900"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="2"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                  >
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <rect x="8" y="8" width="12" height="12" rx="2" />
+                                    <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
+                                  </svg>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="inset-0 flex items-center" aria-hidden="true">
+                            <div className="w-full border-t border-gray-200" />
+                          </div>
+                          <div className="rounded-lg overflow-hidden">
+                            <div className="relative grid gap-8 bg-white p-5">
                               <a
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}
+                                className="-m-3 p-3 flex items-start cursor-pointer rounded-lg text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
                                 onClick={removeProfile}
                               >
                                 Sign Out
                               </a>
-                            )}
-                          </Menu.Item>
-                        </Menu.Items>
+                            </div>
+                          </div>
+                        </Popover.Panel>
                       </Transition>
-                    </Menu>
+                    </Popover>
                   </>
                 )}
               </div>
