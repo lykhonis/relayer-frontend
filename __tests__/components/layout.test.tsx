d328fe3f5812b104389ead4bf8d95f0600f0f769
Volodymyr Lykhonis <volodymyr@lykhonis.com>
2022-07-07 00:19:36 -0400

    Basic template and styling

---
+++ __tests__/components/layout.test.tsx
@@ -0,0 +1,34 @@
+import { render, waitFor } from '@testing-library/react'
+import Layout from 'components/Layout'
+
+jest.mock('next/head', () => {
+  return {
+    __esModule: true,
+    default: ({ children }: { children: Array<React.ReactElement> }) => {
+      return <>{children}</>
+    }
+  }
+})
+
+describe('Layout Component', () => {
+  it('matches the snapshot', async () => {
+    render(
+      <Layout>
+        <div>test</div>
+      </Layout>
+    )
+    await waitFor(() => {
+      expect(screen).toMatchSnapshot()
+    })
+  })
+  it('renders the page layout with the correct document title', async () => {
+    render(
+      <Layout title="Test document title">
+        <div>App</div>
+      </Layout>
+    )
+    await waitFor(() => {
+      expect(document.title).toEqual('Test document title')
+    })
+  })
+})
